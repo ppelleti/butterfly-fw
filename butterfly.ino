1ae11bdf37b310349f5f20cb13a5dbbcfa80bd2c
Patrick Pelletier <code@funwithsoftware.org>
2018-01-21 15:53:24 -0800

    First draft.

--- butterfly.ino
+++ butterfly.ino
@@ -6,6 +6,7 @@
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, LED_PIN, NEO_RGB + NEO_KHZ800);
 
 uint8_t colors[4][3];
+uint8_t fade;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,11 +15,18 @@ void setup() {
   strip.show(); // Initialize all pixels to 'off'
 
   pinMode(POT_PIN, INPUT);
+
+  for (uint8_t i = 0; i < 4; i++) {
+    randomColor(colors[i]);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  uint16_t brightness = 1023;
+  showColors(brightness);
+  advance(4);
 }
 
 void makeColor(uint8_t v, uint8_t n1, uint8_t n2, uint8_t rgb[3]) {
@@ -43,8 +51,8 @@ void randomColor(uint8_t rgb[3]) {
   makeColor (v, n1, n2);
 }
 
-void blendColors(uint8_t brightness, uint8_t blend, const uint8_t rgb1[3], const uint8_t rgb2[3], uint8_t rgb[]) {
-  for (i = 0; i < 3; i++) {
+void blendColors(uint16_t brightness, uint8_t blend, const uint8_t rgb1[3], const uint8_t rgb2[3], uint8_t rgb[]) {
+  for (uint8_t i = 0; i < 3; i++) {
     uint32_t accum1, accum2;
     accum1 = rgb1[i];
     accum2 = rgb2[i];
@@ -52,8 +60,44 @@ void blendColors(uint8_t brightness, uint8_t blend, const uint8_t rgb1[3], const
     accum2 *= (255 - blend);
     accum1 += accum2;
     accum1 *= brightness;
-    accum1 >>= 16;
+    accum1 >>= 18;
     rgb[i] = accum1;
   }
+}
+
+void computeColors(uint16_t brightness, uint8_t out[3][3]) {
+  for (uint8_t i = 0; i < 3; i++) {
+    blendColors(brightness, fade, colors[i], colors[i+1], out[i]);
+  }
+}
+
+void advance(uint8_t inc) {
+  uint8_t newFade = fade + inc;
+
+  if (newFade < fade) {
+    for (int8_t i = 3; i >= 0; i--) {
+      for (uint8_t j = 0; i < 3; j++) {
+        colors[i+1][j] = colors[i][j];
+      }
+      randomColor(colors[0]);
+    }
+
+    fade = newFade;
+  }
+}
+
+void setColor(uint8_t pixNo, const uint8_t c[3]) {
+  strip.setPixelColor(pixNo, strip.Color(c[0], c[1], c[2]));
+}
+
+void showColors(uint16_t brightness) {
+  uint8_t result[3][3];
+  computeColors(brightness, result);
+  setColor(2, result[0]);
+  setColor(5, result[0]);
+  setColor(1, result[1]);
+  setColor(6, result[1]);
+  setColor(0, result[2]);
+  setColor(7, result[2]);
 }
 
