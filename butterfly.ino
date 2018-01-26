937749c15d29d34e0c9b5515d678c85634b48c89
Patrick Pelletier <code@funwithsoftware.org>
2018-01-26 12:13:51 -0800

    Use gamma correction and read brightness pot.

--- butterfly.ino
+++ butterfly.ino
@@ -13,6 +13,25 @@ Pixel colors[4];
 Pixel result[3];
 uint8_t fade;
 
+// https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+const uint8_t PROGMEM gamma8[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -29,7 +48,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  uint16_t brightness = 128;
+  uint16_t brightness = analogRead(POT_PIN);
   showColors(brightness);
   advance(4);
   delay(20);
@@ -60,6 +79,7 @@ void randomColor(uint8_t rgb[3]) {
 void blendColors(uint16_t brightness, uint8_t blend, const uint8_t rgb1[3], const uint8_t rgb2[3], uint8_t rgb[3]) {
   for (uint8_t i = 0; i < 3; i++) {
     uint32_t accum1, accum2;
+    uint8_t linear;
     accum1 = rgb1[i];
     accum2 = rgb2[i];
     accum1 *= blend;
@@ -67,7 +87,8 @@ void blendColors(uint16_t brightness, uint8_t blend, const uint8_t rgb1[3], cons
     accum1 += accum2;
     accum1 *= brightness;
     accum1 >>= 18;
-    rgb[i] = accum1;
+    linear = accum1;
+    rgb[i] = pgm_read_byte(&gamma8[linear]);
   }
 }
 
